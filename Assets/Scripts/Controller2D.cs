c1e190f4ccbeb3d6c9717f56ef825da14da351cc
Tom Bloom <thomasbloom25@gmail.com>
2018-12-30 15:31:13 +0000

    Initial file and folder structure

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
