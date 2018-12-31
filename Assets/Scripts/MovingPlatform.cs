728153a646509375312572efd1eb5c6599a7ff4a
Tom Bloom <thomasbloom25@gmail.com>
2018-12-31 14:47:21 +0000

    Moving platforms
    There is a divide by zero exception occurring and I can't track down why.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,28 +4,83 @@ using UnityEngine;
 
 public class MovingPlatform : RayCastController {
 
-    public Vector2 move;
+    public float moveSpeed;
     public LayerMask passengerMask;
 
     public Vector2[] wayPoints;
+    public bool isCyclic;
+    public float waitTime;
+    [Range(0, 2)]
+    public float smoothAmount;
+
+    private Vector2[] globalWayPoints;
+    private int fromIndex;
+    private float wayPointPercentage; // 0 - 1
+    private float nextMoveTime;
 
     private List<PassengerMovement> passengerMovement;
     private Dictionary<Transform, Controller2D> passengerDictionary = new Dictionary<Transform, Controller2D>();
 
 	public override void Start () {
         base.Start();
-	}
+
+        globalWayPoints = new Vector2[wayPoints.Length];
+
+        for (int i = 0; i < wayPoints.Length; i++) {
+            globalWayPoints[i] = wayPoints[i] + (Vector2)transform.position;
+        }
+    }
 	
 	void Update () {
         UpdateRayCastOrigins();
 
-        Vector2 velocity = move * Time.deltaTime;
+        Vector2 velocity = CalculatePlatformMovement();
 
         CalculatePassengerMovement(velocity);
         MovePassengers(true);
         transform.Translate(velocity);
         MovePassengers(false);
 	}
+
+    private float SmoothMovement(float x) {
+        float a = smoothAmount + 1;
+        return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1-x, a));
+    }
+
+    private Vector2 CalculatePlatformMovement() {
+        if(Time.time < nextMoveTime) {
+            return Vector2.zero;
+        }
+
+        print("Global: " + globalWayPoints.Length);
+        fromIndex %= globalWayPoints.Length;
+
+        int toIndex = (fromIndex + 1) % globalWayPoints.Length;
+
+        float distanceBetween = Vector2.Distance(globalWayPoints[fromIndex], globalWayPoints[toIndex]);
+        wayPointPercentage += Time.deltaTime * moveSpeed / distanceBetween;
+        wayPointPercentage = Mathf.Clamp01(wayPointPercentage);
+
+        float smoothedPercentage = SmoothMovement(wayPointPercentage);
+
+        Vector2 newPos = Vector2.Lerp(globalWayPoints[fromIndex], globalWayPoints[toIndex], smoothedPercentage);
+
+        // Reached next waypoint
+        if(wayPointPercentage >= 1) {
+            wayPointPercentage = 0;
+            fromIndex++;
+
+            if (!isCyclic) {
+                if (fromIndex >= globalWayPoints.Length - 1) {
+                    fromIndex = 0;
+                    System.Array.Reverse(globalWayPoints);
+                }
+            }
+
+            nextMoveTime = Time.time + waitTime;
+        }
+        return newPos - (Vector2)transform.position;
+    }
 
     private void MovePassengers(bool beforeMovePlatform) {
         foreach (PassengerMovement passenger in passengerMovement) {
@@ -133,6 +188,15 @@ public class MovingPlatform : RayCastController {
     }
 
     private void OnDrawGizmos() {
-        
+        if(wayPoints != null) {
+            Gizmos.color = Color.red;
+            float size = 0.25f;
+
+            for(int i = 0; i < wayPoints.Length; i++) {
+                Vector2 globalPointPos = (Application.isPlaying) ? globalWayPoints[i] : wayPoints[i] + (Vector2)transform.position;
+                Gizmos.DrawLine(globalPointPos - Vector2.up * size, globalPointPos + Vector2.up * size);
+                Gizmos.DrawLine(globalPointPos - Vector2.left * size, globalPointPos + Vector2.left * size);
+            }
+        }
     }
 }
